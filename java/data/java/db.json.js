7351ab0f829feb173982f41115d5b1b610989b4c
Rahul Mittal <rahulmitt@yahoo.com>
2020-03-04 10:06:56 +0530

    sql queries

--- java/data/java/db.json.js
+++ java/data/java/db.json.js
@@ -11,12 +11,39 @@ var db_que = [
 	
 	{
 		question : "SQL",
-		tags : ["qqq"]
+		tags : ["SQL"]
 	},
 ]
 
 var db_ans = [
-    {
-        "text": "qqqqqqqqqq"
-    },
+	{   /* Normalisation */
+		"text" : function(){/*
+qqq
+		*/}.toString().slice(14,-3)
+	},
+
+	{   /* Indexes */
+		"text" : function(){/*
+www
+		*/}.toString().slice(14,-3)
+	},
+
+	{   /* SQL */
+		"text" : function(){/*
+<p>Find nth highest salary</p>
+<pre>
+select name, salary from emp e1 where N-1 = (
+    select count(distinct salary) from emp e2 where e2.salary > e1.salary
+)
+</pre
+<p>Find duplicate rows</p>
+<pre>
+Select * from EMP a where row_id != select max(row_id) from EMP b where a.EMP_ID=b.EMP_ID;
+</pre
+<p>Delete duplicate rows</p>
+<pre>
+Delete * from EMP a where row_id != select max(row_id) from EMP b where a.EMP_ID=b.EMP_ID;
+</pre
+		*/}.toString().slice(14,-3)
+	},
 ]
