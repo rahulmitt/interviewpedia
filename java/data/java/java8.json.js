b4a585520667eef712169b9dd266adfa7f773486
Rahul Mittal <rahulmitt@yahoo.com>
2020-03-29 12:30:21 +0530

    functional interface

--- java/data/java/java8.json.js
+++ java/data/java/java8.json.js
@@ -42,7 +42,7 @@ var java8_que = [
 ]
 
 var java8_ans = [
-    {
+    {   /* Lambda Expressions */
         "text" : function(){/*
 <h1>Lambda Expressions</h1>
 <p>It is an anonymous function that does not have:</p>
@@ -127,43 +127,135 @@ str -> str.length();
         */}.toString().slice(14,-3)
     },
 
-    {
+    {   /* Functional Interfaces */
+        "text" : function(){/*
+<h1 style="text-align: justify;">Functional Interface</h1>
+<p style="text-align: justify;">Functional Interfaces are used to invoke Lambda expressions.</p>
+<p style="text-align: justify;">If an interface has <strong>single abstract method</strong>, such an interface is a Functional Interface.</p>
+<table style="height: 99px;" width="429">
+<tbody>
+<tr>
+<td style="text-align: center;"><strong>Interface</strong></td>
+<td style="text-align: center;"><strong>Single Abstract Method</strong></td>
+</tr>
+<tr>
+<td>Runnable</td>
+<td>run()</td>
+</tr>
+<tr>
+<td>Callable</td>
+<td>call()</td>
+</tr>
+<tr>
+<td>ActionListener</td>
+<td>actionPerformed()</td>
+</tr>
+<tr>
+<td>Comparable</td>
+<td>compareTo()</td>
+</tr>
+</tbody>
+</table>
+<p style="text-align: justify;">It can have any number of <strong>default</strong> and <strong>static</strong> methods. This restriction is only applicable for abstract methods.</p>
+<table style="height: 42px;" width="563">
+<tbody>
+<tr>
+<td style="text-align: center;"><strong>Valid</strong></td>
+<td style="text-align: center;"><strong>Valid</strong></td>
+<td style="text-align: center;"><strong>Invalid</strong></td>
+</tr>
+<tr>
+<td style="vertical-align: top;">
+<pre>
+public interface Interface1 {
+    public void foo();
+}
+</pre>
+</td>
+<td style="vertical-align: top;">
+<pre>
+public interface Interface2 {
+    public void foo();
+
+    default void bar() {
+        // some impl
+    }
+
+    public static void baz() {
+        // some impl
+    }
+}
+</pre>
+</td>
+<td style="vertical-align: top;">
+<pre>
+public interface Interface3 {
+    public void foo();
+    public void bar();
+}
+</pre>
+</td>
+</tr>
+</tbody>
+</table>
+<p style="text-align: justify;">In order to indicate that an interface is a functional interface, java introduced an annotation: <strong>@FunctionalInterface</strong></p>
+<p style="text-align: justify;">Conceptually, a functional interface has exactly one abstract method. Since default methods have an implementation, they are not abstract. If an interface declares an abstract method overriding one of the public methods of java.lang.Object, that also does not count toward the interface's abstract method count since any implementation of the interface will have an implementation from java.lang.Object or elsewhere.</p>
+<pre>
+@FunctionalInterface        // Compile-time error: Multiple non-overriding abstract methods found in interface Foo
+public interface Foo {
+    public void bar();
+    public void baz();
+}
+</pre>
+<p style="text-align: justify;">Following interface declaration satisfies the rules of a functional interface:</p>
+<pre>
+@FunctionalInterface            // No compile-time error
+public interface Interface2 {
+    public void foo();
+
+    default void bar() {
+        // some impl
+    }
+
+    public static void baz() {
+        // some impl
+    }
+}
+</pre>
+        */}.toString().slice(14,-3)
+    },
+
+    {   /* Default methods in Interfaces */
         "text" : function(){/*
 qqqqqqqq1
         */}.toString().slice(14,-3)
     },
 
-    {
+    {   /* Static methods in Interfaces */
         "text" : function(){/*
 qqqqqqqq1
         */}.toString().slice(14,-3)
     },
 
-    {
+    {   /* Predefined Functional Interfaces */
         "text" : function(){/*
 qqqqqqqq1
         */}.toString().slice(14,-3)
     },
 
-    {
+    {   /* Method & Constructor reference using double-colon (::) operator */
         "text" : function(){/*
 qqqqqqqq1
         */}.toString().slice(14,-3)
     },
 
-    {
+    {   /* Stream API */
         "text" : function(){/*
 qqqqqqqq1
         */}.toString().slice(14,-3)
     },
 
-    {
-        "text" : function(){/*
-qqqqqqqq1
-        */}.toString().slice(14,-3)
-    },
-
-    {
+    {   /* Date & Time API */
         "text" : function(){/*
 qqqqqqqq1
         */}.toString().slice(14,-3)
