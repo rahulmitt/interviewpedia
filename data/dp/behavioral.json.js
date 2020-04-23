b834b8c184e39d5390d534315acf7c95051ec5ff
Rahul Mittal <rahulmitt@yahoo.com>
2020-04-23 22:31:07 +0530

    version 2

---
+++ data/dp/behavioral.json.js
@@ -0,0 +1,161 @@
+var behavioral_que = [
+	{
+		question : "Chain of Responsibility",
+		tags : ["GoF", "Behavioral Design Patterns", "Chain of Responsibility"]
+	},
+
+	{
+		question : "Command",
+		tags : ["GoF", "Behavioral Design Patterns", "Command"]
+	},
+
+	{
+		question : "Interpreter",
+		tags : ["GoF", "Behavioral Design Patterns", "Interpreter"]
+	},
+
+	{
+		question : "Mediator",
+		tags : ["GoF", "Behavioral Design Patterns", "Mediator"]
+	},
+
+	{
+		question : "Iterator",
+		tags : ["GoF", "Behavioral Design Patterns", "Iterator"]
+	},
+
+	{
+		question : "Momento",
+		tags : ["GoF", "Behavioral Design Patterns", "Momento"]
+	},
+
+	{
+		question : "Observer",
+		tags : ["GoF", "Behavioral Design Patterns", "Observer"]
+	},
+
+	{
+		question : "State",
+		tags : ["GoF", "Behavioral Design Patterns", "State"]
+	},
+
+	{
+		question : "Strategy",
+		tags : ["GoF", "Behavioral Design Patterns", "Strategy"]
+	},
+
+	{
+		question : "Template Method",
+		tags : ["GoF", "Behavioral Design Patterns", "Template Method"]
+	},
+
+	{
+		question : "Visitor",
+		tags : ["GoF", "Behavioral Design Patterns", "Visitor"]
+	},
+
+	{
+		question : "Null Object",
+		tags : ["GoF", "Behavioral Design Patterns", "Null Object"]
+	},
+]
+
+var behavioral_ans = [
+    {   /* Chain of Responsibility */
+        "text" : function(){/*
+<h1>Chain of Responsibility</h1>
+<p style="text-align: justify;">TODO</p>
+<p><img src="data/dp/images/behavioral/1.chain_of_responsibility.png" alt="" width="800" height="400"/></p>
+        */}.toString().slice(14,-3)
+    },
+
+    {   /* Command */
+        "text" : function(){/*
+<h1>Command</h1>
+<p style="text-align: justify;">TODO</p>
+<p><img src="data/dp/images/behavioral/2.command.png" alt="" width="800" height="400"/></p>
+        */}.toString().slice(14,-3)
+    },
+
+    {   /* Interpreter */
+        "text" : function(){/*
+<h1>Interpreter</h1>
+<p style="text-align: justify;">TODO</p>
+<p><img src="data/dp/images/behavioral/3.interpreter.png" alt="" width="800" height="400"/></p>
+        */}.toString().slice(14,-3)
+    },
+
+    {   /* Mediator */
+        "text" : function(){/*
+<h1>Mediator</h1>
+<p style="text-align: justify;">TODO</p>
+<p><img src="data/dp/images/behavioral/4.mediator.png" alt="" width="800" height="400"/></p>
+        */}.toString().slice(14,-3)
+    },
+
+    {   /* Iterator */
+        "text" : function(){/*
+<h1>Iterator</h1>
+<p style="text-align: justify;">TODO</p>
+<p><img src="data/dp/images/behavioral/5.iterator.png" alt="" width="800" height="400"/></p>
+        */}.toString().slice(14,-3)
+    },
+
+    {   /* Momento */
+        "text" : function(){/*
+<h1>Momento</h1>
+<p style="text-align: justify;">TODO</p>
+<p><img src="data/dp/images/behavioral/6.momento.png" alt="" width="800" height="400"/></p>
+        */}.toString().slice(14,-3)
+    },
+
+    {   /* Observer */
+        "text" : function(){/*
+<h1>Observer</h1>
+<p style="text-align: justify;">TODO</p>
+<p><img src="data/dp/images/behavioral/7.observer.png" alt="" width="800" height="400"/></p>
+        */}.toString().slice(14,-3)
+    },
+
+    {   /* State */
+        "text" : function(){/*
+<h1>State</h1>
+<p style="text-align: justify;">TODO</p>
+<p><img src="data/dp/images/behavioral/8.state.png" alt="" width="800" height="400"/></p>
+        */}.toString().slice(14,-3)
+    },
+
+    {   /* Strategy */
+        "text" : function(){/*
+<h1>Strategy</h1>
+<p style="text-align: justify;">TODO</p>
+<p><img src="data/dp/images/behavioral/9.strategy.png" alt="" width="800" height="400"/></p>
+        */}.toString().slice(14,-3)
+    },
+
+    {   /* Template Method */
+        "text" : function(){/*
+<h1>Template Method</h1>
+<p style="text-align: justify;">TODO</p>
+<p><img src="data/dp/images/behavioral/10.template_method.png" alt="" width="800" height="400"/></p>
+        */}.toString().slice(14,-3)
+    },
+
+    {   /* Visitor */
+        "text" : function(){/*
+<h1>Visitor</h1>
+<p style="text-align: justify;">TODO</p>
+<p><img src="data/dp/images/behavioral/11.visitor.png" alt="" width="800" height="400"/></p>
+        */}.toString().slice(14,-3)
+    },
+
+    {   /* Null Object */
+        "text" : function(){/*
+<h1>Null Object</h1>
+<p style="text-align: justify;">TODO</p>
+<p><img src="data/dp/images/behavioral/12.null_object.png" alt="" width="800" height="400"/></p>
+        */}.toString().slice(14,-3)
+    },
+
+
+]
