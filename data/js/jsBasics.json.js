7daf4e4caf612ed64861fcef1565289dea2846a9
Rahul Mittal <rahulmitt@yahoo.com>
2020-04-24 23:24:45 +0530

    Basic Javascript

--- data/js/jsBasics.json.js
+++ data/js/jsBasics.json.js
@@ -1,7 +1,7 @@
 var jsBasics_que = [
 	{
-		question : "Variables, Constants & Comments",
-		tags : ["Variables", "Constants", "Comments"]
+		question : "Variables & Constants",
+		tags : ["Variables", "Constants", "Let", "Var", "Const"]
 	},
 
 	{
@@ -46,17 +46,66 @@ var jsBasics_que = [
 ]
 
 var jsBasics_ans = [
-    {   /* Variables, Constants & Comments */
+    {   /* Variables & Constants */
         "text" : function(){/*
-<h1>Variables, Constants & Comments</h1>
-<p style="text-align: justify;">TODO</p>
+<h1>Variables & Constants</h1>
+<p style="text-align: justify;">Variables in JS can be defined using <code>Let</code> or <code>Var</code> keywords; whereas,
+a constant can be created using <code>Const</code> keyword</p>
+<p style="text-align: justify;"><a href="https://jsfiddle.net/interviewpedia/c72ep0hf/" target="_blank">jsFiddle: Let, Var
+&amp; Const</a></p>
+
+<h2>Let vs Var</h2>
+<p style="text-align: justify;">The main difference between <code>Let</code> and <code>Var</code> is the scoping rule.
+Variables declared by <code>Var</code> keyword are scoped to the immediate function body (hence the function scope) while
+<code>Let</code> variables are scoped to the immediate enclosing block denoted by { } (hence the block scope):</p>
+<p style="text-align: justify;"><a href="https://jsfiddle.net/interviewpedia/zL9ok1wu/" target="_blank">jsFiddle: Let vs Var</a></p>
         */}.toString().slice(14,-3)
     },
 
     {   /* Data Types */
         "text" : function(){/*
 <h1>Data Types</h1>
-<p style="text-align: justify;">TODO</p>
+<table width="100%">
+<tbody>
+<tr>
+<td>Number</td>
+<td>1, 2, 3, 100, 3.14</td>
+</tr>
+<tr>
+<td>String</td>
+<td>'Hello World'&nbsp;&nbsp;&nbsp;"rahul.mittal@interviewpedia.com"</td>
+</tr>
+<tr>
+<td>Boolean</td>
+<td>true / false</td>
+</tr>
+<tr>
+<td>Null</td>
+<td>Explictily set a variable with no value</td>
+</tr>
+<tr>
+<td>Undefined</td>
+<td>For variables that have not yet been defined</td>
+</tr>
+<tr>
+<td>Object</td>
+<td>Complex data structures - Arrays, Dates, Literals, etc</td>
+</tr>
+<tr>
+<td>Symbol</td>
+<td>Used with Objects</td>
+</tr>
+</tbody>
+</table>
+
+<h2>Numbers</h2>
+<p style="text-align: justify;"><a href="https://jsfiddle.net/interviewpedia/syp16g4k/" target="_blank">jsFiddle: Numbers</a></p>
+
+<h2>Strings</h2>
+<p style="text-align: justify;"><a href="https://jsfiddle.net/interviewpedia/k8e6Ln5r/" target="_blank">jsFiddle: Strings</a></p>
+
+<h2>Template Strings</h2>
+<p style="text-align: justify;"><a href="https://jsfiddle.net/interviewpedia/Lb7q86c3/" target="_blank">jsFiddle: Template Strings</a></p>
         */}.toString().slice(14,-3)
     },
 
