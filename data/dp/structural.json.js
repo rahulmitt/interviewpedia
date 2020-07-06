6fb8550b876135f8b3b1493abd2c7b3affef974f
Rahul Mittal <rahulmitt@yahoo.com>
2020-07-06 23:31:27 +0530

    structural - adapter design pattern

--- data/dp/structural.json.js
+++ data/dp/structural.json.js
@@ -46,58 +46,146 @@ var structural_ans = [
     {   /* Adapter */
         id : 1,
 		"text" : function(){/*
-<p style="text-align: justify;">TODO</p>
-<p><img src="data/dp/images/structural/1.1.class_adapter.png" alt="" width="800" height="400"/></p>
+<h3 style="text-align: justify;">Intent</h3>
+<p style="text-align: justify;">The <strong>Adapter</strong> pattern converts the interface of the <strong>adaptee-class</strong>
+into the <strong>target-interface</strong> that a client requires. It acts as an intermediary and lets classes work together
+that couldn’t otherwise because of an incompatible interface. The Adapter pattern is also known as <strong>Wrapper</strong>.</p>
+<h3 style="text-align: justify;">Benefits</h3>
+<ol>
+	<li style="text-align: justify;">It allows two or more previously incompatible objects to interact.</li>
+	<li style="text-align: justify;">It allows reusability of existing functionality.</li>
+</ol>
+<h3 style="text-align: justify;">Usecases</h3>
+<ol>
+	<li style="text-align: justify;">An object needs to utilize an existing class with an incompatible interface.</li>
+	<li style="text-align: justify;">You want to create a reusable class that cooperates with classes that don’t necessarily have compatible interfaces.</li>
+</ol>
+
+<p style="text-align: justify;">There are two variations of this pattern:</p>
+<ol>
+	<li style="text-align: justify;"><strong>Class Adapter</strong> — adapter <strong>extends (is-a)</strong> adaptee
+	and implements the target interface</li>
+	<li style="text-align: justify;"><strong>Object Adapter</strong> — adapter is <strong>composed of (has-a)</strong> adaptee
+	and implements the target interface</li>
+</ol>
+<p><img src="data/dp/images/structural/1.1.class_adapter.png" alt="" width="100%"/></p>
 <p style="text-align: justify;">&nbsp;</p>
-<p><img src="data/dp/images/structural/1.2.object_adapter.png" alt="" width="800" height="400"/></p>
+	<p><img src="data/dp/images/structural/1.2.object_adapter.png" alt="" width="100%"/></p>
+<p>Reference: <a href="https://github.com/rahulmitt/design-patterns/tree/master/structural/src/main/java/com/rahulmitt/interviewpedia/dp/structural/adapter" target="_blank">Github — Adapter Design Pattern</a></p>
         */}.toString().slice(14,-3)
     },
 
     {   /* Bridge */
         id : 2,
 		"text" : function(){/*
+<h3 style="text-align: justify;">Intent</h3>
 <p style="text-align: justify;">TODO</p>
-<p><img src="data/dp/images/structural/2.bridge.png" alt="" width="800" height="400"/></p>
+<h3 style="text-align: justify;">Benefits</h3>
+<ol>
+	<li style="text-align: justify;">TODO</li>
+</ol>
+<h3 style="text-align: justify;">Usecases</h3>
+<ol>
+	<li style="text-align: justify;">TODO</li>
+</ol>
+<p><img src="data/dp/images/structural/2.bridge.png" alt="" width="100%"/></p>
+
+<p>Reference: <a href="TODO" target="_blank">Github — Bridge Design Pattern</a></p>
         */}.toString().slice(14,-3)
     },
 
 	{   /* Composite */
 		id : 3,
 		"text" : function(){/*
+<h3 style="text-align: justify;">Intent</h3>
 <p style="text-align: justify;">TODO</p>
-<p><img src="data/dp/images/structural/4.composite.png" alt="" width="800" height="400"/></p>
+<h3 style="text-align: justify;">Benefits</h3>
+<ol>
+	<li style="text-align: justify;">TODO</li>
+</ol>
+<h3 style="text-align: justify;">Usecases</h3>
+<ol>
+	<li style="text-align: justify;">TODO</li>
+</ol>
+<p><img src="data/dp/images/structural/3.composite.png" alt="" width="100%"/></p>
+
+<p>Reference: <a href="TODO" target="_blank">Github — Composite Design Pattern</a></p>
         */}.toString().slice(14,-3)
 	},
 
     {   /* Decorator */
         id : 4,
 		"text" : function(){/*
+<h3 style="text-align: justify;">Intent</h3>
 <p style="text-align: justify;">TODO</p>
-<p><img src="data/dp/images/structural/3.decorator.png" alt="" width="800" height="400"/></p>
+<h3 style="text-align: justify;">Benefits</h3>
+<ol>
+	<li style="text-align: justify;">TODO</li>
+</ol>
+<h3 style="text-align: justify;">Usecases</h3>
+<ol>
+	<li style="text-align: justify;">TODO</li>
+</ol>
+<p><img src="data/dp/images/structural/4.decorator.png" alt="" width="100%"/></p>
+
+<p>Reference: <a href="TODO" target="_blank">Github — Decorator Design Pattern</a></p>
         */}.toString().slice(14,-3)
     },
 
     {   /* Facade */
         id : 5,
 		"text" : function(){/*
+<h3 style="text-align: justify;">Intent</h3>
 <p style="text-align: justify;">TODO</p>
-<p><img src="data/dp/images/structural/5.facade.png" alt="" width="800" height="400"/></p>
+<h3 style="text-align: justify;">Benefits</h3>
+<ol>
+	<li style="text-align: justify;">TODO</li>
+</ol>
+<h3 style="text-align: justify;">Usecases</h3>
+<ol>
+	<li style="text-align: justify;">TODO</li>
+</ol>
+<p><img src="data/dp/images/structural/5.facade.png" alt="" width="100%"/></p>
+
+<p>Reference: <a href="TODO" target="_blank">Github — Facade Design Pattern</a></p>
         */}.toString().slice(14,-3)
     },
 
     {   /* Flyweight */
         id : 6,
 		"text" : function(){/*
+<h3 style="text-align: justify;">Intent</h3>
 <p style="text-align: justify;">TODO</p>
-<p><img src="data/dp/images/structural/6.flyweight.png" alt="" width="800" height="400"/></p>
+<h3 style="text-align: justify;">Benefits</h3>
+<ol>
+	<li style="text-align: justify;">TODO</li>
+</ol>
+<h3 style="text-align: justify;">Usecases</h3>
+<ol>
+	<li style="text-align: justify;">TODO</li>
+</ol>
+<p><img src="data/dp/images/structural/6.flyweight.png" alt="" width="100%"/></p>
+
+<p>Reference: <a href="TODO" target="_blank">Github — Flyweight Design Pattern</a></p>
         */}.toString().slice(14,-3)
     },
 
     {   /* Proxy */
         id : 7,
 		"text" : function(){/*
+<h3 style="text-align: justify;">Intent</h3>
 <p style="text-align: justify;">TODO</p>
-<p><img src="data/dp/images/structural/7.proxy.png" alt="" width="800" height="400"/></p>
+<h3 style="text-align: justify;">Benefits</h3>
+<ol>
+	<li style="text-align: justify;">TODO</li>
+</ol>
+<h3 style="text-align: justify;">Usecases</h3>
+<ol>
+	<li style="text-align: justify;">TODO</li>
+</ol>
+<p><img src="data/dp/images/structural/7.proxy.png" alt="" width="100%"/></p>
+
+<p>Reference: <a href="TODO" target="_blank">Github — Proxy Design Pattern</a></p>
         */}.toString().slice(14,-3)
     },
 
