52a7ebce844c62c7ce255cc0c36e41675fd33a14
Rahul <rahulmitt@yahoo.com>
2023-04-15 17:43:22 +0530

    library mgmt system

--- data/system_design/ooad_puzzles.json.js
+++ data/system_design/ooad_puzzles.json.js
@@ -110,48 +110,30 @@ var ooad_puzzles_ans = [
 <li>
 <p style="text-align: justify;">Any library member should be able to search books by their title, author, subject category as well by the publication date.</p>
 </li>
-</ol>
-<ol>
 <li>
 <p style="text-align: justify;">Each book will have a unique identification number and other details including a rack number which will help to physically locate the book.</p>
 </li>
-</ol>
-<ol>
 <li>
 <p style="text-align: justify;">There could be more than one copy of a book, and library members should be able to check-out and reserve any copy. We will call each copy of a book, a book item.</p>
 </li>
-</ol>
-<ol>
 <li>
 <p style="text-align: justify;">The system should be able to retrieve information like who took a particular book or what are the books checked-out by a specific library member.</p>
 </li>
-</ol>
-<ol>
 <li>
 <p style="text-align: justify;">There should be a maximum limit (5) on how many books a member can check-out.</p>
 </li>
-</ol>
-<ol>
 <li>
 <p style="text-align: justify;">There should be a maximum limit (10) on how many days a member can keep a book.</p>
 </li>
-</ol>
-<ol>
 <li>
 <p style="text-align: justify;">The system should be able to collect fines for books returned after the due date.</p>
 </li>
-</ol>
-<ol>
 <li>
 <p style="text-align: justify;">Members should be able to reserve books that are not currently available.</p>
 </li>
-</ol>
-<ol>
 <li>
 <p style="text-align: justify;">The system should be able to send notifications whenever the reserved books become available, as well as when the book is not returned within the due date.</p>
 </li>
-</ol>
-<ol>
 <li>
 <p style="text-align: justify;">Each book and member card will have a unique barcode. The system will be able to read barcodes from books and members’ library cards.</p>
 </li>
